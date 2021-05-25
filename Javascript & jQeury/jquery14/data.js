94619d4fb2f08fb80b458d868881334dcbb6c80e
DoUn Kim <dududada0717@gmail.com>
2021-05-25 21:51:13 +0900

    Add files via upload

---
+++ Javascript & jQeury/jquery14/data.js
@@ -0,0 +1,24 @@
+// 미리 준비한 틀에 출력될 내용을 JSON으로 준비
+var content = {
+    speak: {
+        data: [
+            { eng: "A piece of cake.", kor: "식은 죽 먹기지요" },
+            { eng: "Absolutely.", kor: "절대적으로 그렇지요" },
+            { eng: "After you.", kor: "먼저 가시지요" },
+            { eng: "Be my guest.", kor: "사양하지 말고 하세요" },
+            { eng: "Be patient.", kor: "좀 참으세요" },
+            { eng: "Be punctual", kor: "시간좀 지켜" },
+            { eng: "Catch you later.", kor: "나중에 보자구요" },
+            { eng: "Certainly.", kor: "확실히 그래요" },
+            { eng: "Delicious", kor: "맛있어요" },
+            { eng: "Did you get it?", kor: "알아 들었어요?" },
+            { eng: "Easy does it.", kor: "천천히 하는 것이 잘 하는 거에요" },
+            { eng: "Either will do.", kor: "둘 다 되요" },
+            { eng: "Enjoy your meal.", kor: "맛있게 드세요" },
+            { eng: "Far from it.", kor: "아직 멀었지요" },
+            { eng: "Follow me.", kor: "따라 오세요" },
+            { eng: "Get in the line.", kor: "줄을 서세요" },
+            { eng: "Hang on.", kor: "잠깐 기다리세요" }
+        ]
+    }
+};
